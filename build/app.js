224086ea4a46932f5ca60dc7ab8f32fa85d0d842
ndxbxrme <lewis_the_cat@hotmail.com>
2017-02-21 19:39:59 +0000

    null

--- build/app.js
+++ build/app.js
@@ -2,9 +2,12 @@
   'use strict';
   module.exports = function(ndx) {
     return ndx.app.get('/api/memory', ndx.authenticate('superadmin'), function(req, res) {
-      return res.json({
+      var mem;
+      mem = {
         memory: process.memoryUsage().rss / 1048576
-      });
+      };
+      res.json(mem);
+      return mem = null;
     });
   };
 
--- src/app.coffee
+++ src/app.coffee
@@ -2,5 +2,7 @@
 
 module.exports = (ndx) ->
   ndx.app.get '/api/memory', ndx.authenticate('superadmin'), (req, res) ->
-    res.json
+    mem =
       memory: process.memoryUsage().rss / 1048576
+    res.json mem
+    mem = null
